23275072605a30252b8b135a055e1163b181ea8e
Jethet <henriette.hettinga@gmail.com>
2020-12-04 16:49:11 +0100

    Add index.html, first app.js and css code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="home-page">
+        <section className="heroe-banner">
+          <div className="container">
+            <div className="heroe-banner-text">
+              <h1>
+                Somos el podcast <strong>La bocacalle</strong>
+              </h1>
+              <h3>Un espacio de expresión para personas sin hogar</h3>
+              <div className="heroe-banner-bottom">
+                <p>Escucha los episodios a tu medida</p>
+                {/* <a href="#" className="btn">Ir al podcast</a> */}
+              </div>
+            </div>
+            <div className></div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
