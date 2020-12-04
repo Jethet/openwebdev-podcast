561294a6cf2db57f15d20bbd91a34715f01425b5
Jethet <henriette.hettinga@gmail.com>
2020-12-04 18:02:53 +0100

    Add clean index.html and temporary text in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="home-page">
-        <section className="heroe-banner">
-          <div className="container">
-            <div className="heroe-banner-text">
-              <h1>
-                Somos el podcast <strong>La bocacalle</strong>
-              </h1>
-              <h3>Un espacio de expresión para personas sin hogar</h3>
-              <div className="heroe-banner-bottom">
-                <p>Escucha los episodios a tu medida</p>
-                {/* <a href="#" className="btn">Ir al podcast</a> */}
-              </div>
-            </div>
-            <div className></div>
-          </div>
-        </section>
+    <div>
+      <div className="temporary-text">
+        <h1>La Bocacalle</h1>
+        
+        <h2>Un espacio de expresión para personas sin hogar</h2>
       </div>
     </div>
   );
