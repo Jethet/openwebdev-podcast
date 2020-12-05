d4fb590ad7a4ca0d030c7e6badd621f4f0c09634
Jethet <henriette.hettinga@gmail.com>
2020-12-05 12:41:48 +0100

    Commit new branch section-presentamos-jet

---
+++ frontend/src/components/Presentamos.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
+
+const Presentamos = () => {
+  return (
+    <div>
+      <Container fluid>
+        <Jumbotron>
+          <p>
+            
+          </p>
+        </Jumbotron>
+      </Container>
+    </div>
+  );
+};
+
+export default Presentamos;
