8959caf5fbd2d27893a7ea4f5010e51164c4dccc
Jethet <henriette.hettinga@gmail.com>
2020-12-05 18:01:50 +0100

    Add images, fonts and styling, add button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css"
+import Presentamos from "./components/Presentamos"
 
 function App() {
   return (
     <div>
-      <div className="temporary-text">
-        <h1>La Bocacalle</h1>
-        <h2>Un espacio de expresión para personas sin hogar</h2>
-      </div>
+      <Presentamos />
     </div>
   );
 }
