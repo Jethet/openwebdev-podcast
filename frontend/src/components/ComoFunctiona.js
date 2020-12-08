1181c5bc7341551127af1fcc5bc737b121b3a391
Jethet <henriette.hettinga@gmail.com>
2020-12-08 16:25:06 +0100

    Finalize fields with speakers and styling

--- frontend/src/components/ComoFunctiona.js
+++ frontend/src/components/ComoFunctiona.js
@@ -22,45 +22,94 @@ const ComoFunctiona = () => {
             </label>
             <input type="text" placeholder="Type here" className="input-field" />
           </section>
+
           <div className="form-text-box">
             <p className="form-text">Hablamos de muchos temas en cada episodio</p>
           </div>
+
           <section className="form-group">
             <label>
               <input type="text" placeholder="Type here" className="input-field" />
             </label>
             <label>
-              <input type="text" style={{color: "white", backgroundColor: "black", border: "2px solid black",  height: "30px"}} />
+              <input
+                type="text"
+                style={{
+                  color: "white",
+                  backgroundColor: "black",
+                  border: "2px solid black",
+                  height: "30px",
+                }}
+              />
             </label>
             <label>
               <input type="text" placeholder="Type here" className="input-field" />
             </label>
             <label>
-              <input type="text" style={{color: "white", backgroundColor: "black", border: "2px solid black",  height: "30px"}} />
+              <input
+                type="text"
+                style={{
+                  color: "white",
+                  backgroundColor: "black",
+                  border: "2px solid black",
+                  height: "30px",
+                }}
+              />
             </label>
           </section>
+
           <div className="form-text-box">
             <p className="form-text">Puedes eligir la/s sección/es que te interesan</p>
           </div>
+
           <section className="form-group">
-            <label htmlFor="field-nine">
+            <label>
               <input type="text" placeholder="Type here" className="input-field" />
             </label>
-            <label htmlFor="field-ten">
-              <input type="text" style={{color: "white", backgroundColor: "black", border: "2px solid black",  height: "30px"}} />
-            </label>
-            <label htmlFor="field-eleven">
+            <div>
+              <img src="/images/speaker.png" className="speaker" />
+              <label>
+                <input
+                  type="text"
+                  className="input-field-speaker"
+                  style={{
+                    color: "white",
+                    backgroundColor: "black",
+                    border: "2px solid black",
+                    height: "30px",
+                  }}
+                />
+              </label>
+            </div>
+
+            <label>
               <input type="text" placeholder="Type here" className="input-field" />
             </label>
-            <label htmlFor="field-twelve">
-              <input type="text" style={{color: "white", backgroundColor: "black", border: "2px solid black",  height: "30px"}} />
-            </label>
+            <div>
+              <img src="/images/speaker.png" className="speaker" />
+              <label>
+                <input
+                  type="text"
+                  className="input-field-speaker"
+                  style={{
+                    color: "white",
+                    backgroundColor: "black",
+                    border: "2px solid black",
+                    height: "30px",
+                  }}
+                />
+              </label>
+            </div>
           </section>
+
           <div className="last-text-box">
             <p className="form-text">Escucha tu selección</p>
           </div>
         </form>
-        <button className="form-button">IR AL PODCAST</button>
+        <button className="form-button">
+          <img src="/images/bar-chart.png" alt="" className="soundbars" />
+          IR AL PODCAST
+        </button>
       </div>
     </>
   );
