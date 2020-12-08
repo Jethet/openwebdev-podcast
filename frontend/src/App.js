8c7bd73923f5dfccf58f79f0f9d73d079fc63f15
Jethet <henriette.hettinga@gmail.com>
2020-12-08 14:20:35 +0100

    Add div to app.js header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,20 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css"
-import Header from "./components/Header"
-import Presentamos from "./components/Presentamos"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Presentamos from "./components/Presentamos";
 
-import Colaboran from "./components/Colaboran"
+import Colaboran from "./components/Colaboran";
 
 import Footer from "./components/Footer";
-
 
 function App() {
   return (
     <div>
-
       <Header />
-      <Presentamos/>
+      <Presentamos />
       <Colaboran />
-
-</div>
+    </div>
   );
 }
-
-
-
 
 export default App;
