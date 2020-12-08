a0c0c1d24996c0fab2f1208f0a51fecf278d819d
Jethet <henriette.hettinga@gmail.com>
2020-12-08 12:00:40 +0100

    Section 'Como Functiona' of home page with React Bootstrap

--- frontend/src/components/Presentamos.js
+++ frontend/src/components/Presentamos.js
@@ -18,33 +18,48 @@ const Presentamos = () => {
         </div>
         <h1 className="presentamos-alfredo-header">ALFREDO</h1>
         <div className="presentamos-alfredo-text">
-          <p>
-            Vengo del seno de una familia que emigró de Andalucía a Cataluña, en busca de
-            una vida digna y huyendo del hambre de la posguerra. Tuve una infancia
-            envidiable en todos los sentidos, me dieron la mejor educación escolar qué
-            creían, en colegios de pago.
-          </p>
-          <p>
-            He vivido en la calle un par de veces por culpa de mis adicciones y por eso me
-            gusta compartir mi vida turbulenta para sensibilizar el impacto de last
-            drogas.
-          </p>
+          <div>
+            <p>
+              Vengo del seno de una familia que emigró de Andalucía a Cataluña, en busca
+              de una vida digna y huyendo del hambre de la posguerra. Tuve una infancia
+              envidiable en todos los sentidos, me dieron la mejor educación escolar qué
+              creían, en colegios de pago.
+            </p>
+          </div>
+          <div>
+            <p>
+              He vivido en la calle un par de veces por culpa de mis adicciones y por eso
+              me gusta compartir mi vida turbulenta para sensibilizar el impacto de last
+              drogas.
+            </p>
+          </div>
         </div>
         <div className="presentamos-img2">
           <Image src="/images/fran.jpg" fluid alt="Fran" />
         </div>
         <h1 className="presentamos-fran-header">FRAN</h1>
         <div className="presentamos-fran-text">
+        <div>
           <p>
             Soy nativo de Sevilla. Me considero una persona divertida y puedo decir que
             soy el relaciones públicas del Raval en Barcelona.
           </p>
+          </div>
+          <div>
           <p>
             Me gusta pasar el tiempo leyendo poesía, cantando flamenco y paseando por el
             barrio.
           </p>
+          </div>
         </div>
-          <Button variant="dark" size="lg" className="black-button" style={{ background: "#000000", border: "#000000", fontWeight: "bold" }}>CONOCE EL PROYECTO</Button>
+        <Button
+          variant="dark"
+          size="lg"
+          className="black-button"
+          style={{ background: "#000000", border: "#000000", fontWeight: "bold" }}
+        >
+          CONOCE EL PROYECTO
+        </Button>
       </Container>
     </>
   );
